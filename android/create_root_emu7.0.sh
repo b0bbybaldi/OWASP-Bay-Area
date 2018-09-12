046a34149790ff99a1c5378c9f3695c8882f6664
Jeroen Willemsen <jwillemsen@xebia.com>
2018-09-12 23:35:41 +0200

    #17: got emulator with busyboxy and xposed (added android 7)

---
+++ android/create_root_emu7.0.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo "This script is based on http://www.andnixsh.com/2018/06/how-to-install-xposed-on-any-android.html"
+echo "Start your emulator with 'emulator -avd NAMEOFX86A7.0 -writable-system -selinux disabled -wipe-data'"
+adb root && adb remount
+adb install SuperSU\ v2.79.apk
+adb push root_avd-master/SuperSU/x86/su /system/xbin/su
+adb shell chmod 0755 /system/xbin/su
+adb shell setenforce 0
+adb shell su --install
+adb shell su --daemon&
+adb push busybox /data/busybox
+# adb shell "mount -o remount,rw /system && mv /data/busybox /system/bin/busybox && chmod 755 /system/bin/busybox && /system/bin/busybox --install /system/bin"
+adb shell chmod 755 /data/busybox
+adb shell 'sh -c "./data/busybox --install /data"'
+adb shell 'sh -c "mkdir /sdcard/xposed"'
+adb push xposed7.zip /data/xposed/xposed.zip
+adb shell chmod 0755 /data/xposed
+adb shell 'sh -c "./data/unzip /data/xposed/xposed.zip -d /data/xposed/"'
+adb shell 'sh -c "cp /data/xposed/xposed/META-INF/com/google/android/*.* /data/xposed/xposed/"'
+echo "Now start the su ./data/xposed/xposed/flash-script.sh as ADB shell after installing SUperSU"
+echo "Next, restart emulator"
+echo "Next, adb install XposedInstaller_3.1.5.apk"
+echo "Next, run installer and then adb reboot"
+# adb shell 'sh -c "su ./sdcard/xposed/xposed/Flash-Script.sh"'
