57a48bf1fc00d01280b92f97190a0b1c2dcaf211
Jeroen Willemsen <jwillemsen@xebia.com>
2018-09-12 22:56:47 +0200

    #17: got emulator with busyboxy and xposed

--- android/create_root_emu.sh
+++ android/create_root_emu.sh
@@ -11,5 +11,13 @@ adb push busybox /data/busybox
 # adb shell "mount -o remount,rw /system && mv /data/busybox /system/bin/busybox && chmod 755 /system/bin/busybox && /system/bin/busybox --install /system/bin"
 adb shell chmod 755 /data/busybox
 adb shell 'sh -c "./data/busybox --install /data"'
-adb push xposed-v90-sdk26-x86-beta3.zip /sdcard
-adb shell 'sh -c ""'
+adb shell 'sh -c "mkdir /sdcard/xposed"'
+adb push xposed.zip /sdcard/xposed/xposed.zip
+adb shell chmod 0755 /sdcard/xposed
+adb shell 'sh -c "./data/unzip /sdcard/xposed/xposed.zip -d /sdcard/xposed/"'
+adb shell 'sh -c "cp /sdcard/xposed/xposed/META-INF/com/google/android/*.* /sdcard/xposed/xposed/"'
+echo "Now start the su ./sdcard/xposed/xposed/Flash-Script.sh as ADB shell after installing SUperSU"
+echo "Next, restart emulator"
+echo "Next, adb install XposedInstaller_3.1.5.apk"
+echo "Next, run installer and then adb reboot"
+# adb shell 'sh -c "su ./sdcard/xposed/xposed/Flash-Script.sh"'
